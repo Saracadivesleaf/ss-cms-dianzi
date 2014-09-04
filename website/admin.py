7aec99cf679c61d47dfbdefb83f63c1de24fafda
Junzki <sswgmxain@gmail.com>
2014-09-05 03:40:21 +0800

    Remove app "website".(Trash code)

--- website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from website.models import Navigation
-
-# Register your models here.
-class NavigationAdmin(admin.ModelAdmin):
-	list_display = ( 'title', 'parent_navigation',)
-
-admin.site.register(Navigation, NavigationAdmin)
